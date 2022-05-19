78f55b8ec78c84d2965769e4369c2c38f0730932
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-05-19 12:21:31 +0530

    Changes again

--- userproject/settings.py
+++ userproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django-heroku
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
